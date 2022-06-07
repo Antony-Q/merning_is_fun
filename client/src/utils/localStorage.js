dd7b9a97c672cc239c00fd520ab31c2b9fefaa8f
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-07 16:18:45 -0500

    fixed localstorage,js

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,52 +1,29 @@
-export const getMe = (token) => {
-    return fetch('/api/users/me', {
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${token}`,
-      },
-    });
-  };
+export const getSavedBookIds = (token) => {
+    const savedBookIds = localStorage.getItem('saved_books')
+        ? JSON.parse(localStorage.getItem('saved_books'))
+        : [];
+    return savedBookIds;
+};
 
-  export const createUser = (userData) => {
-    return fetch('/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
-  };
+export const saveBookIds = (bookIdArr) => {
+    if (bookIdArr.length) {
+        localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
+    } else {
+        localStorage.removeItem('saved_books');
+    }
+};
 
-  export const loginUser = (userData) => {
-    return fetch('/api/users/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
-  };
+export const removeBookId = (bookId) => {
+    const savedBookIds = localStorage.getItem('saved_books')
+        ? JSON.parse(localStorage.getItem('saved_books'))
+        : null;
 
-  export const saveBook = (bookData, token) => {
-    return fetch('/api/users', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(bookData),
-    });
-  };
+    if (!savedBookIds) {
+        return false;
+    }
 
-  export const deleteBook = (bookId, token) => {
-    return fetch(`/api/users/books/${bookId}`, {
-      method: 'DELETE',
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    });
-  };
+    const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
+    localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
 
-  export const searchGoogleBooks = (query) => {
-    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-  };
+    return true;
+};
