98e9ca8695a6288c32a6e2b586e93e14697eeccb
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-07 20:54:00 -0500

    savebook input bug

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -39,22 +39,24 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
-        saveBook: async (parent, { saveBookId }, context) => {
+        saveBook: async (parent, { book }, context) => {
             if (context.user) {
+                console.log(book);
+
                 const updatedUser = await User.findByIdAndUpdate(
                     { _id: context.user._id },
-                    { $push: { savedBooks: saveBookId } },
-                    { new: true }
+                    { $addToSet: { savedBooks: book } },
+                    { new: true, runValidators: true }
                 );
                 return updatedUser;
             }
             throw new AuthenticationError('You need to be logged in!');
         },
-        removeBook: async (parent, { bookId }, context) => {
+        removeBook: async (parent, args, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
                     { new: true }
                 );
                 return updatedUser;
